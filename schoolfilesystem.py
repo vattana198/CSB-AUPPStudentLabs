fcc7ef37882fb3932421b698a33081fabe76d019
altradCS <145883008+altradCS@users.noreply.github.com>
2024-01-15 09:59:51 +0700

    Update and rename studentmanagementsystem.py to schoolfilesystem.py

---
+++ schoolfilesystem.py
@@ -0,0 +1,20 @@
+
+#Libraries you may need:
+import csv, collections, dictionary, (pandas as pd), urlopen, etc..
+
+#classes and Functions to implement
+class SchoolAssessmentSystem:
+    def __init__():
+
+    def process_file():            
+
+    def transfer_data():
+
+    def fetch_web_data():
+
+    def analyze_content():
+
+    def generate_summary():
+
+
+# Analyze content & display result area
